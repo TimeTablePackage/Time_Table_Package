67a8c6c5ecfd2a06a9e07972791d25bcd0379b11
Ayhan <ayhansahin92@gmail.com>
2013-04-23 10:59:15 +0100

    Changes on 23 April

--- TimetablePackage/TimetablePackage/Program.cs
+++ TimetablePackage/TimetablePackage/Program.cs
@@ -1,22 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using Domain;
 
-namespace TimetablePackage
+namespace ConsoleApplication2
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            DataBase mybd = new DataBase();
+            Institute itc = new Institute("it carlow");
+            itc.setBuildingList(mybd.loadBuildingList(itc));
+            itc.setDeptList(mybd.loadDepartmentList(itc));
+
+            Console.Read();
+
         }
     }
 }
