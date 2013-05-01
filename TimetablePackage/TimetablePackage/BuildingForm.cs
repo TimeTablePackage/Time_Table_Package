f933a6e7efa6b8623d5614cb6918c5b6131a137f
ayhan <ayhansahin92@gmail.com>
2013-05-01 22:44:37 +0100

    bla

--- TimetablePackage/TimetablePackage/BuildingForm.cs
+++ TimetablePackage/TimetablePackage/BuildingForm.cs
@@ -23,39 +23,49 @@ namespace TimetablePackage
             // TODO: This line of code loads data into the 'ttpDataSet.Building' table. You can move, or remove it, as needed.
             this.buildingTableAdapter.Fill(this.ttpDataSet.Building);
 
-
-            
-
             if (ttpDataSet.Building.Rows.Count > 0)
             {
-                int rowIndex = 0 ;
+                int rowIndex = 0;
                 int arraySize = ttpDataSet.Building.Rows.Count;
+                int groupBoxNamer = arraySize;
                 String[] groupBoxArray = new String[arraySize];
                 DataRow[] groupArray = new DataRow[arraySize];
 
-
-                for ( rowIndex = 0 ; rowIndex < arraySize ;rowIndex++ )
+                GroupBox[] buildingGroup = new System.Windows.Forms.GroupBox[groupBoxNamer];
+                for (rowIndex = 0; rowIndex < arraySize; rowIndex++)
                 {
-                   groupBoxArray[rowIndex] = ttpDataSet.Building.Rows[rowIndex]["Building_Name"].ToString();
+                    int groupBoxLocater = 0;
+                    groupBoxArray[rowIndex] = ttpDataSet.Building.Rows[rowIndex]["Building_Name"].ToString();
+                    buildingGroup[rowIndex] = new GroupBox();
+                    buildingGroup[rowIndex].Name = groupBoxArray[rowIndex].ToString();
+                    //buildingGroup[groupBoxNamer].BackColor = Color.;
+                    //buildingGroup[groupBoxNamer].ForeColor = Color.Maroon;
+                    buildingGroup[rowIndex].Text = groupBoxArray[rowIndex].ToString();
+                    buildingGroup[rowIndex].Font = new Font("Georgia", 12);
+                   // buildingGroup[rowIndex].location = new System.Drawing.Point(410, 32);
+                    //   buildingGroup[groupBoxNamer].Visible = true;
+                    buildingGroup[rowIndex].Location = new System.Drawing.Point(410, groupBoxLocater + 50);
+                    this.Controls.Add(buildingGroup[rowIndex]);
+
+                    groupBoxLocater = groupBoxLocater + 20;
                 }
 
-                for ( rowIndex = 0 ; rowIndex < arraySize ; rowIndex++)
+            /*    for (rowIndex = 0; rowIndex < arraySize; rowIndex++)
                 {
-                    GroupBox buildingGroup = new GroupBox();
-                    buildingGroup.Name = groupBoxArray[rowIndex].ToString();
-                    buildingGroup.BackColor = Color.LightBlue;
-                    buildingGroup.ForeColor = Color.Maroon;
-                    buildingGroup.Text = "Author Details";
-                    buildingGroup.Font = new Font("Georgia", 12);
+                   
 
-                
-               
-                
+                    //buildingGroup[rowIndex].Name = groupBoxArray[rowIndex].ToString();
+                   // buildingGroup[groupBoxNamer].BackColor = Color.;
+                   // buildingGroup[groupBoxNamer].ForeColor = Color.Maroon;
+                   // buildingGroup[rowIndex].Text = groupBoxArray[rowIndex].ToString();
+                   // buildingGroup[rowIndex].Font = new Font("Georgia", 12);
+                 //   buildingGroup.Location = new System.Drawing.Point(410, 32);
+                 //   buildingGroup[groupBoxNamer].Visible = true;
+                    this.Controls.Add(buildingGroup[rowIndex]);
+              //      groupBoxNamer++;
+                }*/
             }
-            else
-            {
-                MessageBox.Show("Empty");
-            }
+          
         }
     }
 }
